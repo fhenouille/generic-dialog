966f80cf633dfb6a70794e7ce0bb2829ee007e2f
fhenouille <fhenouille@gmail.com>
2025-08-08 18:49:04 +0200

    add use cases, refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,45 @@
 import { useRef, type FunctionComponent } from "react";
-import {
-  GenericDialog,
-  type GenericDialogHandle,
-} from "./components/genericDialog";
+import { ConfirmDialog } from "./components/confirmDialog.component";
+import { type GenericDialogHandle } from "./components/genericDialog.component";
+import { ImageDialog } from "./components/imageDialog.component";
+import { ModalDialog } from "./components/modalDialog.component";
+import { RegularDialog } from "./components/regularDialog.component";
 
 const App: FunctionComponent = () => {
   const modalRef = useRef<GenericDialogHandle>(null);
   const regularRef = useRef<GenericDialogHandle>(null);
+  const confirmRef = useRef<GenericDialogHandle>(null);
+  const imageRef = useRef<GenericDialogHandle>(null);
+
+  const openDialog = (ref: React.RefObject<GenericDialogHandle | null>) => {
+    closeDialogs();
+    ref.current?.open();
+  };
+  const closeDialogs = () => {
+    modalRef.current?.close();
+    regularRef.current?.close();
+    confirmRef.current?.close();
+    imageRef.current?.close();
+  };
 
   return (
     <>
       <h1>Generic Dialog Demo</h1>
-      <button
-        onClick={() => {
-          regularRef.current?.close();
-          modalRef.current?.open();
-        }}
-      >
-        Open Modal Dialog
-      </button>
-      <button onClick={() => regularRef.current?.open()}>
+      <button onClick={() => openDialog(modalRef)}>Open Modal Dialog</button>
+      <button onClick={() => openDialog(regularRef)}>
         Open Regular Dialog
       </button>
-      <GenericDialog
-        ref={modalRef}
-        header={
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h2>Modal Dialog</h2>
-            <button onClick={() => modalRef.current?.close()}>✖</button>
-          </div>
-        }
-        body={
-          <p>
-            The component can be used as a modal
-            <br />
-            (the user has to close it before being able
-            <br />
-            to interact with the rest of the webpage).
-          </p>
-        }
-        footer={<span>Modal Footer</span>}
-      />
-      <GenericDialog
-        ref={regularRef}
-        isModal={false}
-        header={
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h2>Regular Dialog</h2>
-            <button onClick={() => regularRef.current?.close()}>✖</button>
-          </div>
-        }
-        body={
-          <p>
-            The component can be used as a regular dialog
-            <br />
-            (the user can still interact with the rest of the page)
-          </p>
-        }
-        footer={<span>Non-Modal Footer</span>}
-      />
+      <br />
+      <button onClick={() => openDialog(confirmRef)}>
+        Remove Confirmation Dialog
+      </button>
+      <br />
+      <button onClick={() => openDialog(imageRef)}>Open Image Viewer</button>
+
+      <ModalDialog modalRef={modalRef} />
+      <RegularDialog regularRef={regularRef} />
+      <ConfirmDialog confirmRef={confirmRef} />
+      <ImageDialog imageRef={imageRef} />
     </>
   );
 };
---
+++ src/components/confirmDialog.component.tsx
@@ -0,0 +1,28 @@
+import type { FunctionComponent } from "react";
+import {
+  GenericDialog,
+  type GenericDialogHandle,
+} from "./genericDialog.component";
+
+export const ConfirmDialog: FunctionComponent<{
+  confirmRef: React.RefObject<GenericDialogHandle | null>;
+}> = ({ confirmRef }) => {
+  return (
+    <GenericDialog
+      ref={confirmRef}
+      body={
+        <>
+          <h2>Confirm Deletion</h2>
+          <p>
+            Are you sure you want to delete this item? This action cannot be
+            undone.
+          </p>
+          <button onClick={() => confirmRef.current?.close()}>Cancel</button>
+          <button onClick={() => confirmRef.current?.close()} color="error">
+            Delete
+          </button>
+        </>
+      }
+    />
+  );
+};
